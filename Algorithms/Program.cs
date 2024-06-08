bb6aaa59dd0c2a650abb0111de98f97fb1dd77d6
lucas <luccleandro@hotmail.com>
2024-06-08 08:42:22 -0300

    Added fully working bubble sort.

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,6 +6,4 @@ List<int> data = [6, 111, -5, 1, 0, 5, 1, 99, 0];
 
 data = bubbleSort.Sort(data);
 
-foreach (int i in data){
-	Console.WriteLine(i);
-}
+data.ForEach(Console.WriteLine);
--- Algorithms/SortClasses/BubbleSort.cs
+++ Algorithms/SortClasses/BubbleSort.cs
@@ -2,35 +2,38 @@ namespace Algorithms.SortClasses;
 
 public class BubbleSort
 {
-	static List<T> Swap<T>(List<T> list, int firstIndex, int secondIndex)
-	{
-		// Swapping values by tuple deconstruction.
-		(list[firstIndex], list[secondIndex]) = (list[secondIndex], list[firstIndex]);
-		return list;
-	}
+    private static List<T> Swap<T>(List<T> list, int firstIndex, int secondIndex)
+    {
+        // Swapping values by tuple deconstruction.
+        (list[firstIndex], list[secondIndex]) = (list[secondIndex], list[firstIndex]);
+        return list;
+    }
 
-	private List<int> SortUnitIteration(List<int> data)
-	{ 
-		for (int i = 0; i < data.Count - 1; i++)
-		{
-			int firstIndex = i;
-			int secondIndex = i + 1;
+    // Passes one time through entire array, swapping any corresponding elements.
+    private List<int> SortUnitIteration(List<int> data)
+    {
+        for (int i = 0; i < data.Count - 1; i++)
+        {
+            int secondIndex = i + 1;
 
-			if (data[firstIndex] > data[secondIndex])
-			{
-				data = Swap(data, firstIndex, secondIndex);
-			}
-		}
-		return data;
-	}
+            if (data[i] > data[secondIndex])
+            {
+                data = Swap(data, i, secondIndex);
+            }
+        }
 
-	public List<int> Sort(List<int> data)
-	{
-		for (int n = 0; n < data.Count; n++)
-		{
-			data = SortUnitIteration(data);
-		}
-		
-		return data;
-	}
+        return data;
+    }
+
+    // Public method for sorting. Applies one sorting iteration for each data point.
+    // Runs in O(n^2), after all, this IS bubble sort.
+    public List<int> Sort(List<int> data)
+    {
+        for (int n = 0; n < data.Count; n++)
+        {
+            data = SortUnitIteration(data);
+        }
+
+        return data;
+    }
 }

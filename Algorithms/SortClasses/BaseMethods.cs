562b58582ea6f139339cee72de12d01a44573cca
lucas <luccleandro@hotmail.com>
2024-06-09 08:09:06 -0300

    Added SortTwo method

--- Algorithms/SortClasses/BaseMethods.cs
+++ Algorithms/SortClasses/BaseMethods.cs
@@ -2,6 +2,14 @@ namespace Algorithms.SortClasses;
 
 public class HelperMethods
 {
+    internal static List<int> SortTwo(List<int> Data)
+    {
+        if (Data[0] > Data[1])
+            return Swap(Data, 0, 1);
+
+        return Data;
+    }       
+
 	internal static List<T> Swap<T>(List<T> Data, int FirstIndex, int SecondIndex)
     {
         // Swapping values by tuple deconstruction.

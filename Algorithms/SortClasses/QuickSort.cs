5f21ee8a43804e319c2e3d68db5223a0ba18977d
lucas <luccleandro@hotmail.com>
2024-06-10 18:44:40 -0300

    Minor refactoring and added comments

--- Algorithms/SortClasses/QuickSort.cs
+++ Algorithms/SortClasses/QuickSort.cs
@@ -1,17 +1,20 @@
-using System.Runtime.InteropServices;
 using Algorithms.SortClasses;
 
 public class QuickSort : ISortAlgorithm
 {
+	// Public method for sorting.
 	public List<int> Sort(List<int> Data)
-	{
+	{	
 		Conquer(ref Data, 0, Data.Count - 1);
 		return Data;
 	}
 
-	public int Divide(List<int> Data, int Start, int End)
-	{
+	
+	internal int Divide(List<int> Data, int Start, int End)
+	{	
+		// Running in ascending, pivot is the last value.
 		int pivotValue = Data[End];
+		// Index used to store which index should be swapped.
 		int index = Start - 1;
 
 		for (int i = Start; i <= End - 1; i++)
@@ -23,12 +26,15 @@ public class QuickSort : ISortAlgorithm
 			}
 		}
 
+		// Changes pivot place.
 		Data = HelperMethods.Swap(Data, index + 1, End);
 
+		// Returning the next pivot index.
 		return index + 1;
 	}
 
-	public void Conquer(ref List<int> Data, int Start, int End)
+	// Recursive method. Attention: data is passed by reference, not as parameter.
+	internal void Conquer(ref List<int> Data, int Start, int End)
 	{
 		if (Start < End)
 		{

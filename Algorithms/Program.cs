780e45ae343b6949fca47f81e8ba87373ef3ec39
lucas <luccleandro@hotmail.com>
2024-06-10 17:10:36 -0300

    Added Quick Sort

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,9 +1,17 @@
 ﻿using Algorithms.SortClasses;
 
 BubbleSort bubbleSort = new();
+MergeSort mergeSort = new();
+QuickSort quickSort = new();
 
-List<int> data = [6, 111, -5, 1, 0, 5, 1, 99, 0];
+List<int> data = [6, 111, -5, 1, 0, 5, 1, 99];
 
-data = bubbleSort.Sort(data);
+// data = bubbleSort.Sort(data);
+// data.ForEach(Console.WriteLine);
 
+// data = mergeSort.Sort(data);
+// data.ForEach(Console.WriteLine);
+
+
+data = quickSort.Sort(data);
 data.ForEach(Console.WriteLine);

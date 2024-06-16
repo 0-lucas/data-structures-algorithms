b0ddb42c097d437c29dd745249c439b3e22d706d
lucas <luccleandro@hotmail.com>
2024-06-16 11:18:39 -0300

    Added singly linked list to Data Structures directory

---
+++ Algorithms/DataStructures/LinkedLists.cs
@@ -0,0 +1,83 @@
+namespace Algorithms.DataStructures;
+
+public class Element
+{
+    public int Value;
+    public Element? NextElement;
+    public Element? PreviousElement;
+
+    public Element(int Value, Element NextElement)
+    {
+        this.Value = Value;
+        this.NextElement = NextElement;
+    }
+
+    public Element(int Value)
+    {
+        this.Value = Value;
+        this.NextElement = null;
+    }
+}
+
+public class SinglyLinkedList
+{
+    public Element? Head;
+    public int Count;
+
+    public void InsertAtBeginning(int Value)
+    {
+        Element newElement = new(Value);
+        newElement.NextElement = Head;
+        Head = newElement;
+
+        Count++;
+    }
+
+    public void InsertAtEnd(int Value)
+    {
+        if (Head == null) // If linked list is already empty, we can just insert wherever.
+            InsertAtBeginning(Value);
+
+        Element newElement = new(Value);
+        Element lastElement = GetLastElement();
+
+        lastElement.NextElement = newElement;
+        Count++;
+    }
+
+    public Element GetLastElement()
+    {
+        if (Head == null)
+            throw new EntryPointNotFoundException("No HEAD element");
+
+        return GetLastElement(Head, 1);
+    }
+
+    private Element GetLastElement(Element element, int count)
+    {
+        if (count == this.Count)
+            return element;
+
+        count++;
+        return GetLastElement(element.NextElement, count);
+    }
+
+    public void Print()
+    {
+        if (Head == null)
+        {
+            Console.WriteLine("No values found");
+            return;
+        }
+
+        Element? printedElement = Head;
+        for (int i = 0; i < this.Count; i++)
+        {
+            if (printedElement != null) // Just to supress a CS8602 warning. Not actually needed.
+            {
+                Console.WriteLine(printedElement.Value);
+                printedElement = printedElement.NextElement;
+            }
+        }
+    }
+}
